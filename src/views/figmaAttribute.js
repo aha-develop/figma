a795bf67e6a416b14d7b7e163578fb45ba7f4bc4
Percy Hanna <percy@aha.io>
2022-12-30 10:13:55 -0700

    Use EmbeddedContentAttribute component

--- src/views/figmaAttribute.js
+++ src/views/figmaAttribute.js
@@ -1,49 +1,21 @@
 import React from "react";
-import { EmbeddedContent } from "@aha-develop/aha-develop-react";
+import { EmbeddedContentAttribute } from "@aha-develop/aha-develop-react";
 import { ensureEmbedFlags } from "../ensureEmbedFlags";
 
-async function link(record) {
-  await aha.command("aha-develop.figma.figmaLink", { record });
-}
-
-async function unLink(record) {
-  await aha.command("aha-develop.figma.figmaUnLink", { record });
-}
+const identifier = "aha-develop.figma";
 
 aha.on("figmaAttribute", ({ record, fields }) => {
   const url = ensureEmbedFlags(fields.figmaLink);
 
   return (
-    <>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "1fr auto",
-          gridGap: 10,
-        }}
-      >
-        {!fields.figmaLink && <span />}
-        {fields.figmaLink && <EmbeddedContent src={url} />}
-        <aha-menu>
-          <aha-button slot="control" kind="secondary" size="small">
-            <aha-icon icon="fa-solid fa-ellipsis"></aha-icon>
-          </aha-button>
-          <aha-menu-content>
-            <aha-menu-item>
-              <aha-button kind="plain" onClick={() => link(record)}>
-                Link Figma file...
-              </aha-button>
-            </aha-menu-item>
-            {fields.figmaLink && (
-              <aha-menu-item>
-                <aha-button kind="plain" onClick={() => unLink(record)}>
-                  Unlink
-                </aha-button>
-              </aha-menu-item>
-            )}
-          </aha-menu-content>
-        </aha-menu>
-      </div>
-    </>
+    <EmbeddedContentAttribute
+      identifier={identifier}
+      record={record}
+      fields={fields}
+      product="Figma"
+      fieldName="figmaLink"
+      placeholder="Add Figma URL"
+      src={url}
+    />
   );
 });
