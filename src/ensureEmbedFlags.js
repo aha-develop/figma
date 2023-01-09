ec5ecc6fdec02f5a8257ee3a7ed05107e6729043
Jeffery Tucker <jeff@aha.io>
2023-01-09 09:25:21 -0500

    Use embed API for all valid figma links

--- src/ensureEmbedFlags.js
+++ src/ensureEmbedFlags.js
@@ -1,9 +1,8 @@
 export const ensureEmbedFlags = (url) => {
   if (!url) return
 
-  if (url.includes('figma.com/embed')) {
-    return url
-  } else {
-    return `https://www.figma.com/embed?embed_host=share&url=${encodeURIComponent(url)}`
-  }
+  // From https://www.figma.com/developers/embed
+  if (!url.match(/https:\/\/([\w\.-]+\.)?figma.com\/(file|proto)\/([0-9a-zA-Z]{22,128})(?:\/.*)?$/)) return
+
+  return `https://www.figma.com/embed?embed_host=share&url=${encodeURIComponent(url)}`
 }
